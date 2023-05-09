370449eba505f59b8eefc55bd49ea3d226cb5883
Roberta Lacerda <vcindustria@gmail.com>
2023-05-09 14:37:17 -0300

    Atualização

---
+++ clouds.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var randomClouds = document.querySelectorAll(".random-cloud");
+
+//   randomClouds.forEach(function (cloud) {
+//     var randomTop = Math.floor(Math.random() * 100);
+//     var randomRight = Math.floor(Math.random() * 100);
+//     cloud.style.top = randomTop + "%";
+//     cloud.style.right = randomRight + "%";
+//   });
+// });
